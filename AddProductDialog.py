6fa2565c87213269f71ab0373ba6eb2c8a796fcd
NikitaKolotushkin <nikitaplay1000@gmail.com>
2021-12-07 14:10:49 +0400

    Небольшие изменения в функционале

--- AddProductDialog.py
+++ AddProductDialog.py
@@ -5,7 +5,7 @@ import sqlite3
 
 from PyQt5.QtCore import Qt
 from PyQt5 import QtGui, QtMultimedia, uic, QtCore
-from PyQt5.QtWidgets import QDialog, QMainWindow, QApplication, QLabel
+from PyQt5.QtWidgets import QDialog, QMainWindow, QApplication, QLabel, QFileDialog
 
 from db import to_binary, products, product_types
 
@@ -13,21 +13,30 @@ from db import to_binary, products, product_types
 class AddProductDialog(QDialog):
     def __init__(self, parent=None):
         super().__init__()
+        self.file = ''
         uic.loadUi('ui_templates/AddProductDialog.ui', self)
 
         self.setWindowIcon(QtGui.QIcon('src/admin.png'))
 
         self.addProductButton.clicked.connect(self.addProduct)
-
+        self.selectImageButton.clicked.connect(self.selectImage)
         self.cancelButton.clicked.connect(self.close)
+        [self.productTypesList.addItem(type_[1]) for type_ in product_types.getData()]
+
+    def selectImage(self):
+        filename, _ = QFileDialog.getOpenFileName(
+            self, "Выберите изображение", "", "Image Files (*.jpg *.png *.jpeg *.svg)")
+        self.imageName.setText(filename if filename else '')
+        self.file = filename if filename else self.file
 
     def addProduct(self):
-        if self.imageName != '1' and self.productNameInput.text().strip() != '' and \
-                self.productDescriptionInput.text().strip() != '':
+        if self.imageName != '' and self.productNameInput.text().strip() != '' and \
+                self.productDescriptionInput.text().strip() != '' and \
+                self.productTypesList.currentText() != 'Не выбрано':
             products.add(
-                to_binary('admin.png'),
+                to_binary(self.file),
                 self.productNameInput.text(),
                 self.productDescriptionInput.text(),
                 self.productTypesList.currentText(),
-                self.priceInput.value(),)
+                float(self.priceInput.value()),)
             self.close()
--- AdministratorPanel.py
+++ AdministratorPanel.py
@@ -5,7 +5,8 @@ from PyQt5.QtCore import Qt
 from PyQt5 import QtGui, QtMultimedia, uic, QtCore
 from PyQt5.QtWidgets import QDialog, QMainWindow, QApplication, QLabel
 
-from AddProductDialog import *
+from AddProductDialog import AddProductDialog
+from AddCategoryDialog import AddCategoryDialog
 
 
 class AdministratorPanel(QDialog):
@@ -15,9 +16,14 @@ class AdministratorPanel(QDialog):
 
         self.setWindowIcon(QtGui.QIcon('src/admin.png'))
 
-        self.addProductButton.clicked.connect(self.openProductAdding)
+        self.addProductButton.clicked.connect(self.addProduct)
+        self.addCategoryButton.clicked.connect(self.addCategory)
         self.cancelButton.clicked.connect(self.close)
 
-    def openProductAdding(self):
-        addProduct = AddProductDialog()
-        addProduct.exec_()
+    def addCategory(self):
+        add = AddCategoryDialog(self)
+        add.exec_()
+
+    def addProduct(self):
+        add = AddProductDialog(self)
+        add.exec_()
