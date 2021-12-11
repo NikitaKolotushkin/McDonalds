5ecf42c97ddc4fec79f27ac9260608928bcf9fc4
NikitaKolotushkin <nikitaplay1000@gmail.com>
2021-12-11 13:11:07 +0400

    Добавлены строки справочной информации

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ from PyQt5.QtWidgets import QApplication
 from MainWindow import MainWindow
 
 
+__author__ = 'Rensys aka NikitaKolotushkin'
+__version__ = '0.1a'
+__license__ = 'GPL-3.0 License'
+__docformat__ = 'Sphinx & reStructuredText'
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = MainWindow()
